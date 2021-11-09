03405933f9fbefadd35db91b6d05920bbfa1c8cb
jiminojimi <54015448+jiminojimi@users.noreply.github.com>
2021-11-10 00:30:08 +1100

    update start-server

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     ( python manage.py createsuperuser --no-input)
 fi
-(gunicorn martor_demo.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(gunicorn Dashi3.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
